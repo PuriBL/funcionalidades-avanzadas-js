77e54d0413f72a0082c6d6bac66d20c4b4c82d9c
Puri <purificacion.bastida@gmail.com>
2023-11-18 20:58:38 +0100

    Añadir ejer

--- src/ejercicio-09/index.js
+++ src/ejercicio-09/index.js
@@ -1,1 +1,14 @@
-// Escribe tu código aquí:
+const colors = new Set()
+
+colors.add('Rojo')
+colors.add('Verde')
+colors.add('Azul')
+colors.add('Rojo') // Intentar agregar un color duplicado
+
+console.log('Colores en el Set:', colors)
+
+const colorToCheck = 'Verde'
+console.log (`¿${colorToCheck} existe en el Set? ${colors.has(colorToCheck)}`)
+
+
+
