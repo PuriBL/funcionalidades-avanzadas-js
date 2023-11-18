97f5a8872f4a1b294c0390e8a59b0b5b616c4017
Puri <purificacion.bastida@gmail.com>
2023-11-18 21:08:11 +0100

    Añadir ejer 10

--- src/ejercicio-10/index.js
+++ src/ejercicio-10/index.js
@@ -1,1 +1,15 @@
-// Escribe tu código aquí:
+const personas = new Map()
+
+personas.set('Alice', 30)
+personas.set('Bob', 25)
+personas.set('Charlie', 35)
+
+console.log('Edad de Alice:', personas.get('Alice'))
+
+const personToRemove = 'Bob'
+personas.delete(personToRemove)
+
+console.log('personas en el map después de eliminar a Bob:', personas)
+
+
+
