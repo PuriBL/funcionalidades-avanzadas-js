35eb109fc4d766740665f5642de5aa36cfa53ae8
Puri <purificacion.bastida@gmail.com>
2023-11-18 19:51:16 +0100

    Añadir ejer 2

--- src/ejercicio-02/index.js
+++ src/ejercicio-02/index.js
@@ -1,1 +1,22 @@
-// Escribe tu código aquí:
+const person = {
+    name: 'John',
+    greet: function () {
+        console.log(`Hola, soy ${this.name}`)
+    }
+}
+
+person.greet() // Hola, soy John
+
+// Cambiando a una arrow function
+const person2 = {
+    name: 'alice',
+    greet: () => {
+        console.log(`Hola, soy ${this.name}`)
+    }
+}
+
+person2.greet() // Hola, soy undefined (no cambia el valor de 'this')
+
+
+
+
